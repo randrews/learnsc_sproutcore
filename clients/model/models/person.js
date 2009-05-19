b7f83c9190fc957fab27cf90d9016dfaa5df0669
Ross Andrews <randrews@ross-andrewss-macbook.local>
2009-05-19 17:31:13 -0500

    Made save button save to Rails (in prod only)

--- clients/model/models/person.js
+++ clients/model/models/person.js
@@ -14,11 +14,38 @@ require('core');
 */
 Model.Person = SC.Record.extend(
 /** @scope Model.Data.prototype */ {
+  properties: ['firstName','lastName','salary'],
+
   prettyName:function(){
     return this.get('lastName')+", "+this.get('firstName');
   }.property(),
 
   humanName:function(){
     return this.get('firstName')+" "+this.get('lastName');
-  }.property()
+  }.property(),
+
+  commit: function() {
+    if(Model.localMode){
+      return;
+    }
+
+    var self=this;
+
+    sc_super();
+
+    var props=$H(this.getPropertyData()).toJSON();
+    var parameters = { person: props };
+
+    var opts = {
+      method:'put',
+      emulateUncommonMethods: true,
+
+      parameters: parameters,
+      onSuccess: function(transport) {
+        self.updateAttributes(transport.responseJSON);
+      }
+    };
+
+    new Ajax.Request("/people/"+this.get('guid'),opts);
+  }
 }) ;
